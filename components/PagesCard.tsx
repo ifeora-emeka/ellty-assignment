9e0d393e8aea85800d6f39e761f52bbd740984af
Emeka Ifeora <34845973+Emyboy@users.noreply.github.com>
2025-09-19 01:08:31 +0100

    feat: enhance UI components and add button functionality with new styles

---
+++ components/PagesCard.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React, { useState } from 'react'
+import EachPageItem from './EachPageItem';
+import Button from './Button';
+
+type Props = {
+    pages: { id: string; label: string }[];
+}
+
+export default function PagesCard({ pages }: Props) {
+    const [selectedPages, setSelectedPages] = useState<Set<string>>(new Set());
+
+    const handlePageSelection = (id: string, checked: boolean) => {
+        if (id === 'select-all') {
+            if (checked) {
+                setSelectedPages(new Set(pages.map(page => page.id)));
+            } else {
+                setSelectedPages(new Set());
+            }
+        } else {
+            const newSelected = new Set(selectedPages);
+            if (checked) {
+                newSelected.add(id);
+            } else {
+                newSelected.delete(id);
+            }
+            setSelectedPages(newSelected);
+        }
+    };
+
+    const isAllSelected = selectedPages.size === pages.length;
+    const hasSelectedItems = selectedPages.size > 0;
+
+    return (
+        <div
+            className='card md:w-[370px] w-full rounded-[6px] border-[1px] py-[10px] border-border'
+            style={{
+                boxShadow: '0px 8px 15px 0px #1414141F, 0px 0px 4px 0px #1414141A'
+            }}
+        >
+            <EachPageItem
+                id={'select-all'}
+                label={'All pages'}
+                checked={isAllSelected}
+                onChange={handlePageSelection}
+            />
+            <div className='py-[10px] px-[15px]'>
+                <hr />
+            </div>
+            <div className={`flex flex-col`}>
+                {pages.map((page) => (
+                    <EachPageItem
+                        key={page.id}
+                        id={page.id}
+                        label={page.label}
+                        checked={selectedPages.has(page.id)}
+                        onChange={handlePageSelection}
+                    />
+                ))}
+            </div>
+            <div className='py-[10px] px-[15px]'>
+                <hr />
+            </div>
+            <div className='py-[10px] px-[15px]'>
+                <Button fullWidth disabled={!hasSelectedItems}>Done</Button>
+            </div>
+        </div>
+    )
+}
